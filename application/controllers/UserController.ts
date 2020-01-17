fb875e5e74fa4e1738f60c7b25fd607bc6c1aa9b
Luis V <luis.vega@diginteligencia.com>
2020-01-16 18:37:33 -0600

    Add jsonwebtoken dependency

--- application/controllers/UserController.ts
+++ application/controllers/UserController.ts
@@ -2,6 +2,8 @@ import { Inject, Singleton } from 'typescript-ioc';
 import UserService from '../services/UserService';
 import { GenericController, IController } from './GenericController';
 import User from '../database/entity/User';
+import { IRouterContext } from 'koa-router';
+import { sign } from 'jsonwebtoken';
 
 @Singleton
 export default class UserController extends GenericController<User> implements IController {
@@ -10,5 +12,14 @@ export default class UserController extends GenericController<User> implements I
 	constructor(@Inject private userService: UserService) {
 		super();
 		this.service = userService;
+	}
+
+	public async getAll(ctx: IRouterContext) {
+		const page = ctx.query.page || 1;
+		const newToken = sign({ "name": "luis", "lastname": "vega", role: 2 }, 'secret', {
+			expiresIn: "1h"
+		});
+		ctx.set("token", newToken);
+		ctx.body = await this.userService.findAll(page);
 	}
 }
